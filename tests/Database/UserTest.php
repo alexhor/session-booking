7e237422ea4864580a832131562d82b2fa4bff30
Alexander Dennenmoser <alexander.dennenmoser@outlook.de>
2025-01-29 07:11:56 +0000

    Completed user model tests

--- tests/Database/UserTest.php
+++ tests/Database/UserTest.php
@@ -34,4 +34,127 @@ final class UserTest extends CIUnitTestCase
         $user_model->insert($user_data);
         $this->assertCount(2, $user_model->findAll());
     }
+
+    public function testDeleteUser(): void
+    {
+        $user_model = new User();
+
+        $this->assertCount(0, $user_model->findAll());
+
+        $user_data = [
+            'firstname' => 'Test',
+            'lastname' => 'Testo',
+            'email' => 'test.testo@example.com',
+        ];
+        $user_id = $user_model->insert($user_data);
+        $this->assertCount(1, $user_model->findAll());
+
+        $user_model->delete($user_id);
+        $this->assertCount(0, $user_model->findAll());
+    }
+
+    public function testUniqueEmail(): void
+    {
+        $user_model = new User();
+
+        $this->assertCount(0, $user_model->findAll());
+
+        $user_data = [
+            'firstname' => 'Test',
+            'lastname' => 'Testo',
+            'email' => 'test.testo@example.com',
+        ];
+        $user_id = $user_model->insert($user_data);
+        $this->assertCount(1, $user_model->findAll());
+
+        $user_data = [
+            'firstname' => 'asdasfdas',
+            'lastname' => 'dfgdfgdfg',
+            'email' => 'test.testo@example.com',
+        ];
+        $this->expectException(CodeIgniter\Database\Exceptions\DatabaseException::class);
+        $user_model->insert($user_data);
+        $this->assertCount(1, $user_model->findAll());
+    }
+
+    public function testMissingValues(): void
+    {
+        $user_model = new User();
+
+        $this->assertCount(0, $user_model->findAll());
+
+        // Email missing
+        $user_data = [
+            'firstname' => 'Test',
+            'lastname' => 'Testo',
+        ];
+        $this->expectException(CodeIgniter\Database\Exceptions\DatabaseException::class);
+        $user_model->insert($user_data);
+        $this->assertCount(0, $user_model->findAll());
+
+        // Firstname missing
+        $user_data = [
+            'lastname' => 'Testo',
+            'email' => 'test.testo@example.com',
+        ];
+        $this->expectException(CodeIgniter\Database\Exceptions\DatabaseException::class);
+        $user_model->insert($user_data);
+        $this->assertCount(0, $user_model->findAll());
+        
+        // Lastname missing
+        $user_data = [
+            'firstname' => 'Test',
+            'email' => 'test.testo@example.com',
+        ];
+        $this->expectException(CodeIgniter\Database\Exceptions\DatabaseException::class);
+        $user_model->insert($user_data);
+        $this->assertCount(0, $user_model->findAll());
+
+        // All missing
+        $user_data = [];
+        $this->expectException(CodeIgniter\Database\Exceptions\DatabaseException::class);
+        $user_model->insert($user_data);
+        $this->assertCount(0, $user_model->findAll());
+    }
+
+    public function testUpdateUser(): void
+    {
+        $user_model = new User();
+
+        $this->assertCount(0, $user_model->findAll());
+
+        $user_data = [
+            'firstname' => 'Test',
+            'lastname' => 'Testo',
+            'email' => 'test.testo@example.com',
+        ];
+        $user_id = $user_model->insert($user_data);
+        $this->assertCount(1, $user_model->findAll());
+
+        // Only change some values
+        $user_data = [
+            'id' => $user_id,
+            'firstname' => 'Test2',
+            'lastname' => 'Testo2',
+            'email' => 'test.testo@example.com',
+        ];
+        $user_model->save($user_data);
+        $this->assertCount(1, $user_model->findAll());
+        foreach ($user_data as $key => $_) {
+            $this->assertEquals($user_data[$key], $user_model->find($user_id)[$key]);
+        }
+
+        // Change all values
+        $user_data = [
+            'id' => $user_id,
+            'firstname' => 'Tessdfkk kkd',
+            'lastname' => 'Te2',
+            'email' => 'hello@world.de',
+        ];
+        $user_model->save($user_data);
+        $this->assertCount(1, $user_model->findAll());
+        foreach ($user_data as $key => $_) {
+            $this->assertEquals($user_data[$key], $user_model->find($user_id)[$key]);
+        }
+    }
 }
