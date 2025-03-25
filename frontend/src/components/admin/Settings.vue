2df3893ba417772bca62e776ebdfeac50b06d053
Alexander Dennenmoser <alexander.dennenmoser@outlook.de>
2025-03-25 14:05:01 +0000

    Added password toggle

--- frontend/src/components/admin/Settings.vue
+++ frontend/src/components/admin/Settings.vue
@@ -37,6 +37,17 @@ defineProps({
                             <label :for="setting.key + '_usenow'"class="form-check-label">Use current time</label>
                         </span>
                     </div>
+                    <div v-else-if="setting.validation == 'password'" style="position: relative;">
+                        <input v-model="setting.value" class="form-control" :type="shownPasswordInputs.includes(setting.key) ? 'text' : 'password'">
+                        <svg v-if="shownPasswordInputs.includes(setting.key)" @click="shownPasswordInputs.splice(shownPasswordInputs.indexOf(setting.key), 1)" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill show-hide-password-icon" viewBox="0 0 16 16">
+                            <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"/>
+                            <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"/>
+                        </svg>
+                        <svg v-else @click="shownPasswordInputs.push(setting.key)" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-slash-fill show-hide-password-icon" viewBox="0 0 16 16">
+                            <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7 7 0 0 0 2.79-.588M5.21 3.088A7 7 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474z"/>
+                            <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12z"/>
+                        </svg>
+                    </div>
                     <input v-else-if="setting.validation == 'integer'" v-model="setting.value" class="form-control" type="number">
                     <input v-else-if="typeof setting.validation !== 'object'" v-model="setting.value" class="form-control">
                     <select v-else v-model="setting.value" class="form-select">
@@ -62,6 +73,7 @@ export default {
         return {
             __settingsList: {},
             __propsConfigsBefore: [],
+            shownPasswordInputs: [],
         };
     },
     computed: {
